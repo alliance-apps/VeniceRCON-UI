626723ae0dbb16191465c4a8a1a31781935a725b
Tim Vogler <tim@allianceapps.de>
2020-12-26 01:05:31 +0100

    Using wrong route params id

--- src/views/PluginConfig.vue
+++ src/views/PluginConfig.vue
@@ -12,7 +12,7 @@
                         square
                         size="sm"
                         @click="saveSettings()"
-                        :disabled="!$store.getters.hasPermission('PLUGINREPOSITORY#MODIFY', $route.params.idalt)"
+                        :disabled="!$store.getters.hasPermission('PLUGINREPOSITORY#MODIFY', $route.params.id)"
                 >
                     Save
                 </CButton>
